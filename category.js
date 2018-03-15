feb58c9d196ddb507d9cafa6d83e67b25ce9f02f
Xinda Huang <XindaH@github.com>
2018-03-15 12:03:47 -0500

    final version

--- category.js
+++ category.js
@@ -1,5 +1,7 @@
 var title = "";
 var address = "";
+var unit_price1 ;
+var unit_price2 ;
 
 function category1(){
 	address = "images/Shopping.png";
@@ -29,4 +31,20 @@ function category5(){
 function category6(){
 	address = "images/Fitness.png";
 	title = "Fitness";
+}
+
+function makingcomparison(){
+	unit_price1 = document.getElementById("itemprice1").value/document.getElementById("itemsize1").value;
+	unit_price2 = document.getElementById("itemprice2").value/document.getElementById("itemsize2").value;
+	unit_price1 = unit_price1.toFixed(2);
+	unit_price2 = unit_price2.toFixed(2);
+	
+	if(unit_price1 < unit_price2){
+		document.getElementById("outcome").innerHTML = "Our Recommendation: " + " Item1 is a better choice";
+	}	
+	else{
+		document.getElementById("outcome").innerHTML = "Our Recommendation: " + "Item2 is a better choice";	
+	}
+	document.getElementById("unit-price1").innerHTML = "Unit-Price1: " + unit_price1;	
+	document.getElementById("unit-price2").innerHTML = "Unit-Price2: " + unit_price2;
 }
